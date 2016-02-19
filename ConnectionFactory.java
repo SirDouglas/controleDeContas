bbc410a2b355fdc8f104ae5ab9c3fa88e0527e4e
SirDouglas <douglas.armore@gmail.com>
2016-02-18 22:18:34 -0200

    Update ConnectionFactory.java

--- ConnectionFactory.java
+++ ConnectionFactory.java
@@ -9,10 +9,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- *
- * @author tiago
- */
+
 public class ConnectionFactory {
     
     public Connection getConnection() {
